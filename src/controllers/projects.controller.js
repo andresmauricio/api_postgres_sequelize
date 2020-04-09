4c663443d958aa5b1f1434b95dc3d2959a3b5e7d
andresmauricio <andresacelas@outlook.com>
2020-04-09 18:28:21 -0500

    api projects finished

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -1,3 +1,55 @@
-export const getProjects = (req, res) => {
-    res.json('toma tú proyecto!')
-}
+import Project from "../models/Project";
+
+export const getProjects = async (req, res) => {
+  const result = await Project.findAll();
+  res.json(result);
+};
+
+export const getProject = async (req, res) => {
+  const { id } = req.params;
+  const result = await Project.findOne({ where: { id } });
+  res.json(result);
+};
+
+export const createProject = async (req, res) => {
+  const { name, priority, description, deliverydate } = req.body;
+  try {
+    let newProject = await Project.create(
+      {
+        name,
+        priority,
+        description,
+        deliverydate
+      },
+      { fields: ["name", "priority", "description", "deliverydate"] }
+    );
+    if (newProject) {
+      res.json({ result: true, data: newProject });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(false);
+  }
+};
+
+export const deleteProject = async (req, res) => {
+  const { id } = req.params;
+  const count = await Project.destroy({ where: { id } });
+  res.json({ count });
+};
+
+export const updateProject = async (req, res) => {
+  const { id } = req.params;
+  const { name, priority, description, deliverydate } = req.body;
+  const projects = await Project.findAll({
+    attributes: ["id", "name", "priority", "description", "deliverydate"],
+    where: { id }
+  });   
+  if (projects.length > 0) {
+      projects.forEach(async project => {
+          await project.update({name, priority, description, deliverydate})
+      });
+  }
+
+  return res.json(projects)
+};
--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
-import {getProjects} from '../controllers/projects.controller'
+import {createProject, getProjects, getProject, deleteProject, updateProject} from '../controllers/projects.controller'
 const router = Router();
 
-router.get('/', getProjects)
+router.get('/', getProjects);
+router.get('/:id', getProject);
+router.post('/', createProject);
+router.delete('/:id', deleteProject);
+router.put('/:id', updateProject);
 
 export default router;
